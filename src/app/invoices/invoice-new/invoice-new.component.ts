792b61034ef6554fad8ef1af10dafe3c0c9b1957
AleksandrMaltsev <alex.m@2muchcoffee.com>
2019-01-30 11:42:11 +0200

    feat(invoices:invoice-new): display price calculated value depending on product price, quantity and discount, clean code

--- src/app/invoices/invoice-new/invoice-new.component.ts
+++ src/app/invoices/invoice-new/invoice-new.component.ts
@@ -1,16 +1,13 @@
 import { Component, OnInit } from '@angular/core';
-import { FormArray, FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { Router } from '@angular/router';
+import { FormArray, FormControl, FormGroup, Validators } from '@angular/forms';
 
 import { Store } from '@ngxs/store';
-import { Observable } from 'rxjs';
+import { combineLatest, Observable } from 'rxjs';
+import { startWith } from 'rxjs/operators';
 
 import { CustomersService } from '../../core/services/customers.service';
-import { InvoicesService } from '../../core/services/invoices.service';
 import { ProductsService } from '../../core/services/products.service';
-import { AddInvoice } from '../../ngxs/invoices/invoices.actions';
 import { CustomerModel } from '../../shared/models/customer.model';
-import { InvoiceModel } from '../../shared/models/invoice.model';
 import { ProductModel } from '../../shared/models/product.model';
 
 
@@ -23,87 +20,77 @@ export class InvoiceNewComponent implements OnInit {
 
   products$: Observable<ProductModel[]>;
   customers$: Observable<CustomerModel[]>;
- // invoices$: Observable<InvoiceModel[]>;
   invoiceForm;
+  itemsTotal;
 
 
-  constructor(private productsService: ProductsService,
-              private customersService: CustomersService,
-             // private invoicesService: InvoicesService,
-              private router: Router,
-              private fb: FormBuilder,
-              private store: Store
-              ) {
+  constructor(
+    private productsService: ProductsService,
+    private customersService: CustomersService,
+    private store: Store
+  ) {
+  }
+
+  get items() {
+    return this.invoiceForm.get('items') as FormArray;
+  }
+
+  get discount() {
+    return this.invoiceForm.get('discount') as FormControl;
   }
 
   ngOnInit() {
-
-      this.products$ = this.productsService.products$;
-      this.customers$ = this.customersService.customers$;
-     // this.invoices$ = this.invoicesService.invoices$;
-      this.invoiceForm = this.fb.group({
-        _id: [''],
-        customer: ['', Validators.required],
-        products: this.fb.array([
-          this.initProducts()
-        ]),
-        discount: [0, [Validators.min(0), Validators.max(50)]],
-        total: ['']
+    this.products$ = this.productsService.products$;
+    this.customers$ = this.customersService.customers$;
+    this.initForm();
+    this.itemsTotal = combineLatest(
+      this.discount.valueChanges.pipe(startWith(0)),
+      this.items.valueChanges,
+      this.products$
+    ).subscribe(([discount, items, products]) => {
+      const itemForm = items.map(item => {
+        const price = item.product_id ?
+          products.find(p => p._id === item.product_id).price * item.quantity / 100 * (100 - discount) : 0;
+        return item.price = price;
       });
-      this.invoiceForm.get('_id').setValue(
-        this.generateId()
-      );
+
+      this.items.patchValue(itemForm, {emitEvent: false});
+    });
   }
 
   onSubmit() {
-    const date = new Date();
-    console.log((date.getTimezoneOffset()));
-    console.log(date.getDate());
-    console.log(this.invoiceForm.value);
-    this.store.dispatch( new AddInvoice(this.invoiceForm.value));
-    this.router.navigate(['invoices']);
+    console.log(this.items);
+    // this.store.dispatch( new AddInvoice(this.invoiceForm.value));
   }
 
-  initProducts(): FormGroup {
-    return this.fb.group({
-      product : ['', Validators.required],
-      quantity : [1],
+  initForm() {
+    this.invoiceForm = new FormGroup({
+      customer_id: new FormControl(null, Validators.required),
+      items: new FormArray([this.initFormItems()]),
+      discount: new FormControl(0, [Validators.min(0), Validators.max(50), Validators.required]),
+      total: new FormControl(0)
+    });
+  }
+
+  initFormItems(): FormGroup {
+    return new FormGroup({
+      invoice_id: new FormControl(null),
+      product_id: new FormControl(null, Validators.required),
+      quantity: new FormControl(1, [Validators.required, Validators.min(1)]),
+      price: new FormControl(0)
     });
   }
 
   addProductField(): void {
-    const prod = <FormArray>this.invoiceForm.controls.products;
-    prod.push(this.initProducts());
+    this.items.push(this.initFormItems());
   }
 
   deleteProductField(index) {
-    const prod = <FormArray>this.invoiceForm.controls.products;
-    if (prod.length > 1) {
-      prod.removeAt(index);
+    if (this.items.length > 1) {
+      this.items.removeAt(index);
     }
   }
 
-  get quantity() {
-    return this.invoiceForm.get('quantity').value;
-  }
-  get discount() {
-    return this.invoiceForm.get('discount').value;
-  }
-
-  get total() {
-    const itemTtl = this.invoiceForm.controls.products.value
-    .map( prod =>
-      (prod.product.price || 0) * prod.quantity / 100 * (100 - this.discount)
-    );
-    const ttl = itemTtl.reduce((acc, next) =>  acc + next).toFixed(2);
-    this.invoiceForm.get('total').setValue(ttl);
-    return ttl;
-  }
-
-  generateId() {
-    const id = '5i' + Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15);
-    return id;
-  }
 }
 
 
