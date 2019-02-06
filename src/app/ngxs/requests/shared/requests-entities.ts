8b32885fb4a101668866870b2b75404b500ed5cd
AleksandrMaltsev <alex.m@2muchcoffee.com>
2019-02-06 19:03:40 +0200

    refactor(ngxs:requests:shared): change functions to const

--- src/app/ngxs/requests/shared/requests-entities.ts
+++ src/app/ngxs/requests/shared/requests-entities.ts
@@ -1,36 +1,32 @@
-export function requestEntitiesInitial() {
-    return {
-      loading: false,
-      loaded: false,
-      status: '',
-      data: null,
-    };
-  }
+export const requestEntitiesInitial = {
+    loading: false,
+    loaded: false,
+    status: '',
+    data: null,
+};
 
-export function requestEntitiesLoading() {
-    return {
-      loading: true,
-      loaded: false,
-      status: '',
-      data: null,
-    };
-  }
+export const requestEntitiesLoading = {
+  loading: true,
+  loaded: false,
+  status: '',
+  data: null,
+};
 
 export function requestEntitiesSuccess(payload) {
-    return {
-      loading: false,
-      loaded: true,
-      status: 'success',
-      data: payload,
-    };
-  }
+  return {
+    loading: false,
+    loaded: true,
+    status: 'success',
+    data: payload,
+  };
+}
 
 export function requestEntitiesFail(payload) {
-    return {
-      loading: false,
-      loaded: true,
-      status: 'fail',
-      data: payload,
-    };
-  }
+  return {
+    loading: false,
+    loaded: true,
+    status: 'fail',
+    data: payload,
+  };
+}
 
