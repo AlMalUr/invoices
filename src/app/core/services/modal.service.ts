ff65b283ee1202fa792de7136e84376ac0935236
AleksandrMaltsev <alex.m@2muchcoffee.com>
2019-02-08 18:52:27 +0200

    refactor(core:services): formating line breaks and indents

--- src/app/core/services/modal.service.ts
+++ src/app/core/services/modal.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { MatDialog } from '@angular/material';
 
 import { ModalWindowComponent } from '../../modal-window/modal-window.component';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -12,7 +13,7 @@ export class ModalService {
     private dialog: MatDialog
   ) {}
 
-  confirmModal() {
+  confirmModal(): Observable<boolean> {
     return this.dialog.open(ModalWindowComponent, {
       width: '350px'
     })
